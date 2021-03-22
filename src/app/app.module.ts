6a8f81f868682ad355602dabb284c096f6614248
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-22 11:38:16 +0200

    (design): details-card design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { EventDetailsComponent } from './event-details/event-details.component';
 
 // calender
 import { FullCalendarModule } from '@fullcalendar/angular';
+import { DetailsEditComponent } from './details-edit/details-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventDetailsComponent,
     HomeComponent,
+    DetailsEditComponent,
   ],
   imports: [
     BrowserModule,
